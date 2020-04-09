b684ba170a02e9cb976058e074f30bd95eb7615c
Krisnaananta <60454043+Krisnaananta@users.noreply.github.com>
2020-04-09 09:45:57 +0800

    Add files via upload
    menjalankan fitur logout pada taskbar

---
+++ admin/taskbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Taskbar</title>
+</head>
+<body>
+	<div class="taskbar">
+		<div class="profil">
+			<img src="../img/user.png">
+		</div>
+		<ul>
+			<a href="?tampil=user_edit"><li>Profil</li></a>
+		</ul>
+		<ul>
+			<a href="?tampil=petugas"><li>Petugas</li></a>
+		</ul>
+		<ul>
+			<a href="?tampil=gudang"><li>Gudang</li></a>
+		</ul>
+		<ul>
+			<a href="?tampil=barang_masuk"><li>Barang Masuk</li></a>
+		</ul>
+		<ul>
+			<a href="?tampil=barang_keluar"><li>Barang Keluar</li></a>
+		</ul>
+		<ul>
+			<a href="../index.php"><li>Logout</li></a>
+		</ul>
+	</div>
+</body>
+</html>
